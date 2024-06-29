a3278ab10750dfdb0c227ab7af53c9d998e1d8b0
javohirh <khasanovjavokhir2004@gmail.com>
2024-06-29 20:51:04 +0500

    fix

--- app.js
+++ app.js
@@ -10,13 +10,39 @@ loginForm.addEventListener("submit", (e) => {
   let car = 70;
   let plane = 800;
 
-  let footTime = (dis / foot / 1) | 0;
-  let bicycleTime = (dis / bicycle / 1) | 0;
-  let carTime = (dis / car / 1) | 0;
-  let planeTime = (dis / plane / 1) | 0;
+  let footTime = (dis / foot) * 3600;
+  let bycycleTime = (dis / bicycle) * 3600;
+  let carTime = (dis / car) * 3600;
+  let planeTime = (dis / plane) * 3600;
 
-  document.querySelector(".foot").innerHTML = footTime;
-  document.querySelector(".bycyle").innerHTML = bicycleTime;
-  document.querySelector(".car").innerHTML = carTime;
-  document.querySelector(".plane").innerHTML = planeTime;
+  let allHumanHours = Math.floor(footTime / 3600) + " : Soat";
+  let allHumanMinutes = Math.floor((footTime % 3600) / 60) + " : Daqiqa";
+  let allHumanSeconds = Math.floor(footTime % 60) + " : Soniya";
+
+  let allBycycleHours = Math.floor(bycycleTime / 3600) + " : Soat";
+  let allBycycleMinutes = Math.floor((bycycleTime % 3600) / 60) + " : Daqiqa";
+  let allBycycleSeconds = Math.floor(bycycleTime % 60) + " : Soniya";
+
+  let allCarHours = Math.floor(carTime / 3600) + " : Soat";
+  let allCarMinutes = Math.floor((carTime % 3600) / 60) + " : Daqiqa";
+  let allCarSeconds = Math.floor(carTime % 60) + " : Soniya";
+
+  let allPlaneHours = Math.floor(planeTime / 3600) + " : Soat";
+  let allPlaneMinutes = Math.floor((planeTime % 3600) / 60) + " : Daqiqa";
+  let allPlaneSeconds = Math.floor(planeTime % 60) + " : Soniya";
+
+  document.querySelector(".hfoot").innerHTML = allHumanHours;
+  document.querySelector(".mfoot").innerHTML = allHumanMinutes;
+  document.querySelector(".sfoot").innerHTML = allHumanSeconds;
+  document.querySelector(".hbycyle").innerHTML = allBycycleHours;
+  document.querySelector(".mbycyle").innerHTML = allBycycleMinutes;
+  document.querySelector(".sbycyle").innerHTML = allBycycleSeconds;
+
+  document.querySelector(".hcar").innerHTML = allCarHours;
+  document.querySelector(".mcar").innerHTML = allCarMinutes;
+  document.querySelector(".scar").innerHTML = allCarSeconds;
+
+  document.querySelector(".hplane").innerHTML = allPlaneHours;
+  document.querySelector(".mplane").innerHTML = allPlaneMinutes;
+  document.querySelector(".splane").innerHTML = allPlaneSeconds;
 });
